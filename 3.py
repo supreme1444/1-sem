b55569f914b3b87564cbe2ac873f894d8feda3ef
Maxim <supreme14@rambler.ru>
2022-09-03 17:12:17 +0300

    3py

---
+++ 3.py
@@ -0,0 +1,10 @@
+x = int(input("Введите число "))
+y = int(input("Введите число "))
+if x>0 and y<0:
+    print("4")
+elif x>0 and y>0:
+    print("1")
+elif x<0 and y<0:
+    print("3")
+elif x<0 and y>0:
+    print("2")  
