bcc8eb2f379959aef791a9dac8c81d5e55ecf894
Maxim <supreme14@rambler.ru>
2022-09-03 17:13:19 +0300

    5py

---
+++ 5.py
@@ -0,0 +1,7 @@
+from math import sq
+x1 = int(input("Введите число "))
+y1 = int(input("Введите число "))
+x2 = int(input("Введите число "))
+y2 = int(input("Введите число "))
+rast=sq(pow(x1-x2,2) + pow(y1-y2,2))
+print(rast)
